ec1229b0271ec0d4c25f497e95a8571041ea504c
Sundar Anbu <sundaranbu007@gmail.com>
2023-03-06 08:38:21 +0530

    test

--- birthyear.py
+++ birthyear.py
@@ -3,4 +3,4 @@ current_year=datetime.date.today()
 year=current_year.year
 dob=int(input("Enter the year of birth?"))
 bdob=year-dob
-print('Your Age is',bdob)
+print('Your Age is figured out',bdob)
